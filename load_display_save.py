85255513557a216024fabd61b3e66d3818818fa9
mas192 <samjoshva@rediffmail.com>
2019-11-16 00:34:04 -0800

    obama

--- load_display_save.py
+++ load_display_save.py
@@ -4,14 +4,16 @@ import os
 import numpy as np
 from imutils.object_detection import non_max_suppression
 import pytesseract
+
 '''https://www.pyimagesearch.com/2018/09/17/
-opencv-ocr-and-text-recognition-with-tesseract/'''
+opencv-ocr-and-text-recognition-with-tesseract/
+https://datacarpentry.org/image-processing/03-opencv-images/'''
 ap = argparse.ArgumentParser()
 
-#path to image
+# path to image
 ap.add_argument('-i', '--image', required=True,
                 help='Path to image')
-#path to save processed image
+# path to save processed image need to figure this later
 
 '''ap.add_argument('-o','--output', required=True,
                 help='Saved image path')
@@ -19,12 +21,12 @@ os.chdir()
 cv2.imwrite('Processed',)'''
 args = vars(ap.parse_args())
 
-image = cv2.imread(args['image'])
+image: np.ndarray = cv2.imread(args['image'])
 print(f'Width of image is {image.shape[1]} pixels')
 print(f'Height of image is {image.shape[0]} pixels ')
 print(f'The # of channels in image is {image.shape[2]}')
+print(f'The image type is {type(image)}')
 
 cv2.imshow('Original', image)
 cv2.waitKey(0)
-
-
+# cv2.imshow(image, 'modified.png')
